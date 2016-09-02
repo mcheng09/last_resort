b42fc9e1cd8e41a3bf1dc91d7ed3c0202cfa9121
Michael Cheng <mcheng.oc@gmail.com>
2016-09-02 15:36:12 -0700

    adds users route for index, create, show

---
+++ models/user.js
@@ -0,0 +1,13 @@
+// user model
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+  username: String,
+  password: String,
+  image_url: String,
+  email: String
+});
+
+module.exports = mongoose.model('users', User);
